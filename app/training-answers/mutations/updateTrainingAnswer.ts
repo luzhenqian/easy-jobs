d3845b986663318767dccd370e0183d673028150
LuZhenqian <luzhenqian98@gmail.com>
2022-09-24 03:45:52 +0800

    feat: use api replace local launch chrome

--- app/training-answers/mutations/updateTrainingAnswer.ts
+++ app/training-answers/mutations/updateTrainingAnswer.ts
@@ -1,9 +1,7 @@
 import { resolver } from "@blitzjs/rpc"
 import db from "db"
 import { z } from "zod"
-import { toPng } from "html-to-image"
-import chrome from "chrome-aws-lambda"
-import puppeteer from "puppeteer-core"
+import axios from "axios"
 
 const UpdateTrainingAnswer = z.object({
   id: z.number(),
@@ -25,49 +23,42 @@ export default resolver.pipe(
         data,
       })
     } else {
+      const httpClient = axios.create({
+        baseURL: process.env.AWS_REGION
+          ? "https://easy-jobs-screenshot.vercel.app"
+          : "http://localhost:3003",
+      })
+
       const answer = await db.trainingAnswer.findUnique({ where: { id } })
       const { trainingId } = answer!
       const training = await db.training.findFirst({ where: { recordId: trainingId } })
 
-      const options = process.env.AWS_REGION
-        ? {
-            args: chrome.args,
-            executablePath: await chrome.executablePath,
-            headless: chrome.headless,
-          }
-        : {
-            args: [],
-            executablePath:
-              process.platform === "win32"
-                ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-                : process.platform === "linux"
-                ? "/usr/bin/google-chrome"
-                : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-          }
+      const [originImageBinary, targetImageBinary] = await Promise.all([
+        httpClient({
+          method: "post",
+          url: "/api/screenshot",
+          data: {
+            code: `${data.code.html}<style>${data.code.css}</style>`,
+          },
+          responseType: "arraybuffer",
+        }).then((res) => res.data),
+        httpClient({
+          method: "post",
+          url: "/api/screenshot",
+          data: {
+            code: `${(training?.code as any).html}<style>${(training?.code as any).css}</style>`,
+          },
+          responseType: "arraybuffer",
+        }).then((res) => res.data),
+      ])
 
-      const browser = await puppeteer.launch(options)
-      const originPage = await browser.newPage()
-      console.log(data.code.html, "html")
-      console.log(data.code.css, "css")
-
-      await originPage.setContent(`${data.code.html}<style>${data.code.css}</style>`)
-      const originImageBinary: Buffer = (await originPage.screenshot({
-        encoding: "binary",
-      })) as Buffer
-      const targetPage = await browser.newPage()
-      await targetPage.setContent(
-        `${(training?.code as any).html}<style>${(training?.code as any).css}</style>`
-      )
-      const targetImageBinary: Buffer = (await targetPage.screenshot({
-        encoding: "binary",
-      })) as Buffer
       let pass = false
       console.log(originImageBinary, targetImageBinary)
 
       if (originImageBinary.equals(targetImageBinary)) {
         pass = true
       }
-      browser.close()
+
       trainingAnswer = await db.trainingAnswer.update({
         where: { id },
         data: {
