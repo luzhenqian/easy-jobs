1de1d186ffab10075358f0d28cc461c282e091ff
LuZhenqian <luzhenqian98@gmail.com>
2022-09-25 13:09:36 +0800

    feat: do layout

--- app/trainings/components/Do.tsx
+++ app/trainings/components/Do.tsx
@@ -134,26 +134,17 @@ export const Do = () => {
   }, [code, router.query.trainingId, user])
 
   return (
-    <div
-      className="flex flex-col gap-1 bg-[#1e1e1e] text-gray-100"
-      style={{
-        height: "calc(100vh - 40px)",
+    <Layout
+      headerStyle={{
+        height: "40px",
+        backgroundColor: "#1e1e1e",
+        color: "var(--chakra-colors-gray-100)",
+        borderBottom: "1px solid var(--chakra-colors-gray-400)",
       }}
-    >
-      <div className="px-16 py-2">
-        <Heading size={"sm"}>{training.name}</Heading>
-        {/* <div className="flex justify-between px-4">
-          <Stack spacing={4} direction="row">
-            <CheckboxGroup defaultValue={layout} onChange={(e) => setLayout([...(e as string[])])}>
-              <Checkbox value="HTML">HTML</Checkbox>
-              <Checkbox value="CSS">CSS</Checkbox>
-              <Checkbox value="cur">当前效果</Checkbox>
-              <Checkbox value="target">目标效果</Checkbox>
-            </CheckboxGroup>
-          </Stack>
-
+      actions={
+        <div className="flex-1 flex justify-end px-4">
           <div className="flex gap-4">
-            <Button>
+            <Button size={"xs"} bgColor="green.500">
               <Link href={`/trainings/${type}/${trainingId}/history`}>
                 <a>历史记录</a>
               </Link>
@@ -163,125 +154,137 @@ export const Do = () => {
               color={"white"}
               onClick={submit}
               disabled={submitting}
+              size={"xs"}
             >
               提交
             </Button>
           </div>
-        </div> */}
-      </div>
-      <div className="flex w-full flex-1 border-t border-gray-200">
-        {layout.html ? (
-          <div className="flex flex-col flex-1 w-0">
-            <div
-              className="py-2 px-8 text-lg
+        </div>
+      }
+    >
+      <div
+        className="flex flex-col gap-1 bg-[#1e1e1e] text-gray-100"
+        style={{
+          height: "calc(100vh - 40px)",
+        }}
+      >
+        <div className="px-16 py-2">
+          <Heading size={"sm"}>{training.name}</Heading>
+        </div>
+        <div className="flex w-full flex-1 border-t border-gray-200">
+          {layout.html ? (
+            <div className="flex flex-col flex-1 w-0">
+              <div
+                className="py-2 px-8 text-lg
             text-blue-400
             border-r border-gray-200"
+                onClick={() => setLayout({ ...layout, html: !layout.html })}
+              >
+                html
+              </div>
+              <div className="flex-1 border border-gray-400">
+                <Monaco
+                  language="html"
+                  defaultValue={code.html}
+                  onMount={handleHTMLEditorDidMount}
+                  onChange={handleHTMLEditorChange}
+                  loading={<Loading />}
+                  theme="vs-dark"
+                ></Monaco>
+              </div>
+            </div>
+          ) : (
+            <div
+              className="p-2 text-lg
+          text-blue-400
+          border-r border-gray-200 [writing-mode:vertical-lr]"
               onClick={() => setLayout({ ...layout, html: !layout.html })}
             >
               html
             </div>
-            <div className="flex-1 border border-gray-400">
-              <Monaco
-                language="html"
-                defaultValue={code.html}
-                onMount={handleHTMLEditorDidMount}
-                onChange={handleHTMLEditorChange}
-                loading={<Loading />}
-                theme="vs-dark"
-              ></Monaco>
+          )}
+          {layout.css ? (
+            <div className="flex flex-col flex-1 w-0">
+              <div
+                className="py-2 px-8 text-lg
+text-blue-400"
+                onClick={() => setLayout({ ...layout, css: !layout.css })}
+              >
+                css
+              </div>
+              <div className="flex-1  border border-gray-400">
+                <Monaco
+                  language="css"
+                  defaultValue={code.css}
+                  onMount={handleCSSEditorDidMount}
+                  onChange={handleCSSEditorChange}
+                  loading={<Loading />}
+                  theme="vs-dark"
+                ></Monaco>
+              </div>
             </div>
-          </div>
-        ) : (
-          <div
-            className="p-2 text-lg
+          ) : (
+            <div
+              className="p-2 text-lg
           text-blue-400
           border-r border-gray-200 [writing-mode:vertical-lr]"
-            onClick={() => setLayout({ ...layout, html: !layout.html })}
-          >
-            html
-          </div>
-        )}
-        {layout.css ? (
-          <div className="flex flex-col flex-1 w-0">
-            <div
-              className="py-2 px-8 text-lg
-text-blue-400"
               onClick={() => setLayout({ ...layout, css: !layout.css })}
             >
               css
             </div>
-            <div className="flex-1  border border-gray-400">
-              <Monaco
-                language="css"
-                defaultValue={code.css}
-                onMount={handleCSSEditorDidMount}
-                onChange={handleCSSEditorChange}
-                loading={<Loading />}
-                theme="vs-dark"
-              ></Monaco>
+          )}
+          {layout.current ? (
+            <div className="flex flex-col flex-1 w-0">
+              <div
+                className="py-2 px-8 text-lg
+text-blue-400"
+                onClick={() => setLayout({ ...layout, current: !layout.current })}
+              >
+                当前效果
+              </div>
+              <iframe
+                className="bg-white flex-1 border border-gray-400"
+                srcDoc={`${code.html} <style>${code.css}</style>`}
+              ></iframe>
             </div>
-          </div>
-        ) : (
-          <div
-            className="p-2 text-lg
+          ) : (
+            <div
+              className="p-2 text-lg
           text-blue-400
           border-r border-gray-200 [writing-mode:vertical-lr]"
-            onClick={() => setLayout({ ...layout, css: !layout.css })}
-          >
-            css
-          </div>
-        )}
-        {layout.current ? (
-          <div className="flex flex-col flex-1 w-0">
-            <div
-              className="py-2 px-8 text-lg
-text-blue-400"
               onClick={() => setLayout({ ...layout, current: !layout.current })}
             >
               当前效果
             </div>
-            <iframe
-              className="bg-white flex-1 border border-gray-400"
-              srcDoc={`${code.html} <style>${code.css}</style>`}
-            ></iframe>
-          </div>
-        ) : (
-          <div
-            className="p-2 text-lg
+          )}
+          {layout.target ? (
+            <div className="flex flex-col flex-1 w-0">
+              <div
+                className="py-2 px-8 text-lg
+        text-blue-400"
+                onClick={() => setLayout({ ...layout, target: !layout.target })}
+              >
+                目标效果
+              </div>
+              <iframe
+                className="bg-white flex-1 border border-gray-400"
+                srcDoc={`${(training?.code as any).html} <style>${
+                  (training?.code as any).css
+                }</style>`}
+              ></iframe>
+            </div>
+          ) : (
+            <div
+              className="p-2 text-lg
           text-blue-400
           border-r border-gray-200 [writing-mode:vertical-lr]"
-            onClick={() => setLayout({ ...layout, current: !layout.current })}
-          >
-            当前效果
-          </div>
-        )}
-        {layout.target ? (
-          <div className="flex flex-col flex-1 w-0">
-            <div
-              className="py-2 px-8 text-lg
-        text-blue-400"
               onClick={() => setLayout({ ...layout, target: !layout.target })}
             >
               目标效果
             </div>
-            <iframe
-              className="bg-white flex-1 border border-gray-400"
-              srcDoc={`${(training?.code as any).html} <style>${
-                (training?.code as any).css
-              }</style>`}
-            ></iframe>
-          </div>
-        ) : (
-          <div
-            className="p-2 text-lg
-          text-blue-400
-          border-r border-gray-200 [writing-mode:vertical-lr]"
-            onClick={() => setLayout({ ...layout, target: !layout.target })}
-          >
-            目标效果
-          </div>
-        )}
+          )}
+        </div>
       </div>
-    </div>
+    </Layout>
   )
 }
