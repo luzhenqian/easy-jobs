2a6f3d6346f7e6c6c5071f49f980f831b14deecb
LuZhenqian <luzhenqian98@gmail.com>
2022-09-27 19:44:38 +0800

    feat: remove code sharing need authorize

--- app/code-sharings/queries/getCodeSharings.ts
+++ app/code-sharings/queries/getCodeSharings.ts
@@ -1,15 +1,12 @@
-import { paginate } from "blitz";
-import { resolver } from "@blitzjs/rpc";
-import db, { Prisma } from "db";
+import { paginate } from "blitz"
+import { resolver } from "@blitzjs/rpc"
+import db, { Prisma } from "db"
 
 interface GetCodeSharingsInput
-  extends Pick<
-    Prisma.CodeSharingFindManyArgs,
-    "where" | "orderBy" | "skip" | "take"
-  > {}
+  extends Pick<Prisma.CodeSharingFindManyArgs, "where" | "orderBy" | "skip" | "take"> {}
 
 export default resolver.pipe(
-  resolver.authorize(),
+  // resolver.authorize(),
   async ({ where, orderBy, skip = 0, take = 100 }: GetCodeSharingsInput) => {
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
     const {
@@ -21,15 +18,14 @@ export default resolver.pipe(
       skip,
       take,
       count: () => db.codeSharing.count({ where }),
-      query: (paginateArgs) =>
-        db.codeSharing.findMany({ ...paginateArgs, where, orderBy }),
-    });
+      query: (paginateArgs) => db.codeSharing.findMany({ ...paginateArgs, where, orderBy }),
+    })
 
     return {
       codeSharings,
       nextPage,
       hasMore,
       count,
-    };
+    }
   }
-);
+)
