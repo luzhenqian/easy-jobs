fd039dda29c5e04d3d606a08035cc2b56d2ba240
LuZhenqian <luzhenqian98@gmail.com>
2022-10-18 13:07:14 +0800

    feat: get book not need authorize

--- app/books/queries/getBooks.ts
+++ app/books/queries/getBooks.ts
@@ -1,35 +1,28 @@
-import { paginate } from "blitz";
-import { resolver } from "@blitzjs/rpc";
-import db, { Prisma } from "db";
+import { paginate } from "blitz"
+import { resolver } from "@blitzjs/rpc"
+import db, { Prisma } from "db"
 
 interface GetBooksInput
-  extends Pick<
-    Prisma.BookFindManyArgs,
-    "where" | "orderBy" | "skip" | "take"
-  > {}
+  extends Pick<Prisma.BookFindManyArgs, "where" | "orderBy" | "skip" | "take"> {}
 
-export default resolver.pipe(
-  resolver.authorize(),
-  async ({ where, orderBy, skip = 0, take = 100 }: GetBooksInput) => {
-    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const {
-      items: books,
-      hasMore,
-      nextPage,
-      count,
-    } = await paginate({
-      skip,
-      take,
-      count: () => db.book.count({ where }),
-      query: (paginateArgs) =>
-        db.book.findMany({ ...paginateArgs, where, orderBy }),
-    });
+export default resolver.pipe(async ({ where, orderBy, skip = 0, take = 100 }: GetBooksInput) => {
+  // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+  const {
+    items: books,
+    hasMore,
+    nextPage,
+    count,
+  } = await paginate({
+    skip,
+    take,
+    count: () => db.book.count({ where }),
+    query: (paginateArgs) => db.book.findMany({ ...paginateArgs, where, orderBy }),
+  })
 
-    return {
-      books,
-      nextPage,
-      hasMore,
-      count,
-    };
+  return {
+    books,
+    nextPage,
+    hasMore,
+    count,
   }
-);
+})
